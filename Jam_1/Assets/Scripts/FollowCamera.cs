81818c680c3928b32bd81c2a5215945d12fa2540
Aportillog <aportillog92@gmail.com>
2017-11-06 20:34:53 +0100

    Jam #1 - Proyecto base
    Añadidos movimientos de la nave y la cámara.
    Añadido fondo para la skybox

---
+++ Jam_1/Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour {
+
+    [SerializeField]
+    Transform target;
+    [SerializeField]
+    Vector3 defaultDistance = new Vector3(0f, 4f, -10f);
+    [SerializeField]
+    float distanceDamp = 0.15f;
+
+    Transform myT;
+    Vector3 velocity = Vector3.one;
+
+    void Awake()
+    {
+        myT = transform;
+    }
+
+    void LateUpdate()
+    {
+        SmoothFollow();
+
+    }
+
+
+    void SmoothFollow()
+    {
+        Vector3 toPos = target.position + (target.rotation * defaultDistance);
+        Vector3 curPos = Vector3.SmoothDamp(myT.position, toPos, ref velocity, distanceDamp);
+        myT.position = curPos;
+
+        myT.LookAt(target, target.up);
+    }
+}
---
+++ Jam_1/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField]
+    float movementSpeed = 50f;
+    [SerializeField]
+    float sideThursterSpeed = 5f;
+    [SerializeField]
+    float turnSpeed = 60f; 
+
+    Transform myT;
+
+    void Awake()
+    {
+        myT = transform;
+    }
+    void Update()
+    {
+        Thrust();
+        Turn();
+    }
+
+    void Turn()
+    {
+        float yaw = turnSpeed * Time.deltaTime * Input.GetAxis("Yaw");
+        float pitch = turnSpeed * Time.deltaTime * Input.GetAxis("Pitch");
+        float roll = turnSpeed * Time.deltaTime * Input.GetAxis("Roll");
+
+        myT.Rotate(-pitch,yaw,-roll);
+    }
+
+
+    void Thrust()
+    {
+        //Forward
+        if(Input.GetAxis("Vertical") > 0)
+            myT.position += myT.forward * movementSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+        //Side
+        myT.position += myT.right * sideThursterSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+
+    }
+}
