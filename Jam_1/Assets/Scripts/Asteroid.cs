7aa5258da836c0ce00b3e67a436062d43d32bdfa
Aportillog <aportillog92@gmail.com>
2017-11-08 20:18:37 +0100

    Jam #1 - Asteroides añadidos
    Los asteroides se distribuyen random y giran. Falta añadirles collider y
    función para destruir la nave.

---
+++ Jam_1/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour {
+    [SerializeField]
+    float minScale = 0.8f;
+    [SerializeField]
+    float maxScale = 1.2f;
+    [SerializeField]
+    float rotationOffset = 50f;
+    Transform myT;
+    Vector3 randRotation;
+
+    void Awake()
+    {
+        myT = transform;
+    }
+
+    void Start()
+    {
+        //Random Size
+        Vector3 scale = Vector3.one;
+        scale.x = Random.Range(minScale, maxScale) * myT.localScale.x;
+        scale.y = Random.Range(minScale, maxScale) * myT.localScale.y;
+        scale.z = Random.Range(minScale, maxScale) * myT.localScale.z;
+
+        myT.localScale = scale;
+
+        //Random Rotation
+        randRotation.x = Random.Range(-rotationOffset, rotationOffset);
+        randRotation.y = Random.Range(-rotationOffset, rotationOffset);
+        randRotation.z = Random.Range(-rotationOffset, rotationOffset);
+    }
+	void Update()
+    {
+        myT.Rotate(randRotation * Time.deltaTime);
+    }
+}
---
+++ Jam_1/Assets/Scripts/AsteroidManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidManager : MonoBehaviour {
+    [SerializeField]
+    Asteroid asteroid;
+    [SerializeField]
+    int numOfAsteroidsOnAnAxis = 10;
+    [SerializeField]
+    int gridSpacing = 100;
+
+    void Start()
+    {
+        PlaceAsteroids();
+    }
+
+	void PlaceAsteroids()
+    {
+        for(int x = 0; x < numOfAsteroidsOnAnAxis; x++)
+        {
+            for(int y = 0; y < numOfAsteroidsOnAnAxis; y++)
+            {
+                for (int z = 0; z < numOfAsteroidsOnAnAxis; z++)
+                {
+                    InstantiateAsteroid(x, y, z);
+                }
+            }
+            
+        }
+    }
+
+    void InstantiateAsteroid(int x, int y, int z)
+    {
+        Instantiate(asteroid, 
+                    new Vector3(transform.position.x + (x * gridSpacing) + AsteroidOffset(), 
+                                transform.position.y + (y * gridSpacing) + AsteroidOffset(), 
+                                transform.position.z + (z * gridSpacing) + AsteroidOffset()), 
+                        Quaternion.identity, 
+                        transform);
+    }
+
+    float AsteroidOffset()
+    {
+        return Random.Range(-gridSpacing/2f, gridSpacing/2f);
+    }
+}
