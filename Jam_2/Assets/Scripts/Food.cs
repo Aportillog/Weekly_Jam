1072a7978f6e6a4ebdf778f537bcc160b6eed379
Aportillog <aportillog92@gmail.com>
2017-11-20 23:17:12 +0100

    Jam #2 - Sistema de colisiones
    Actualmente funciona el disparo de la brocheta, pero el sistema de
    colisiones hay que pulirlo.
    El siguiente paso podría ser el sistema de puntuación y creación de
    objetivos, para tener algo casi jugable ya.

--- Jam_2/Assets/Scripts/Food.cs
+++ Jam_2/Assets/Scripts/Food.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Food : MonoBehaviour {
 
     public float rotationOffset = 50f;
+    public bool rotationFlag = true;
 
     Vector3 randRotation;
     Transform myT;
+    bool isPicked = false;
 
     void Awake()
     {
@@ -21,7 +23,28 @@ public class Food : MonoBehaviour {
 
     void Update()
     {
-        myT.Rotate(randRotation * Time.deltaTime);
+        FoodRotation();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        PlayerCollision(other);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        //Avoid collision if the collider object is Player or food and is not already picked
+        if(((col.gameObject.tag == "Player") || (col.gameObject.tag == "Food")) && !isPicked)
+        {
+            Debug.Log(this.name + " colliding with " + col.gameObject.name);
+            Physics.IgnoreCollision(col.collider, this.GetComponent<Collider>());
+        }
+        //Otherwise: do stuff
+        else
+        {
+            StopperCollision(col);
+        }
+        
     }
 
     void CalculateRotation()
@@ -29,5 +52,49 @@ public class Food : MonoBehaviour {
         randRotation.x = Random.Range(-rotationOffset, rotationOffset);
         randRotation.y = Random.Range(-rotationOffset, rotationOffset);
         randRotation.z = Random.Range(-rotationOffset, rotationOffset);
+    }
+
+    void FoodRotation()
+    {
+        if (rotationFlag)
+        {
+            myT.Rotate(randRotation * Time.deltaTime);
+        }
+        
+    }
+
+    void PlayerCollision(Collider player)
+    {
+        if(player.tag == "Player")
+        {
+            //Player's head collision
+            if(player.GetType().ToString() == "UnityEngine.SphereCollider")
+            {
+                //Set isPicked true
+                isPicked = true;
+                //Set player as parent
+                this.transform.parent = player.transform;
+                //Set rotation off
+                rotationFlag = false;
+                //Set isTrigger property off (to collide with player bottom and other food)
+                GetComponent<Collider>().isTrigger = false;
+            }
+        }
+    }
+
+    void StopperCollision(Collision col)
+    {
+        if (isPicked)
+        {
+            if (col.gameObject.tag == "Player")
+            {
+                GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            }
+            else
+            {
+                GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            }
+        }
+        
     }
 }
---
+++ Jam_2/Assets/Scripts/Player.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody rb;
+    private Vector3 origPosition;
+    private Collider m_Collider;
+
+    public float speed = 5f;
+    public float skewerRange = 0.5f;
+    public float originalHeight = -1f;
+
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        m_Collider = GetComponent<SphereCollider>();
+        m_Collider.enabled = false;
+    }
+
+    void FixedUpdate()
+    {
+        Move(Input.GetAxis("Horizontal"));
+
+        Shoot();
+    }
+
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        FoodCollision(other);
+    }
+
+    void Move(float horizInput)
+    {
+        Vector3 moveVelocity = rb.velocity;
+        moveVelocity.x = horizInput * speed;
+        rb.velocity = moveVelocity;
+    }
+
+    void Shoot()
+    {
+        origPosition.x = transform.position.x;
+        origPosition.y = originalHeight;
+        origPosition.z = transform.position.z;
+        if (Input.GetButton("Fire1"))
+        {
+            rb.MovePosition(origPosition + new Vector3(0, skewerRange, 0));
+            m_Collider.enabled = true;
+
+        }
+        else
+        {
+            rb.MovePosition(origPosition);
+            m_Collider.enabled = false;
+        }
+            
+
+
+    }
+
+    void FoodCollision(Collider food)
+    {
+        
+    }
+}
