df9c37a6a52285291af90baa1734b4a55b1cac0a
Aportillog <aportillog92@gmail.com>
2017-11-28 21:10:13 +0100

    Jam #3 - Dos primeras tareas terminadas
    Terminadas las tareas:
    - Whitebox de personajes, habitación y armas
    - Mecánica para acusar
    A medias:
    - Transición entre escenas
    He implementado un poco de código para hace rel fadeout/in entre
    escenas, que puede dejarse a un lado desactivado para mejorar en el
    futuro

---
+++ Jam_3/Assets/Scripts/Clickable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clickable : MonoBehaviour {
+
+    private GameController m_scriptGameController;
+    void Start()
+    {
+        m_scriptGameController = GameObject.FindObjectOfType(typeof(GameController)) as GameController;
+    }
+	void OnMouseDown()
+    {
+        m_scriptGameController.ChangeHunchValue(this.gameObject.name, this.gameObject.tag);
+    }
+}
---
+++ Jam_3/Assets/Scripts/GameController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+    public Text weaponInput;
+    public Text suspectInput;
+    public string WinnerSuspect = "Ross";
+    public string WinnerWeapon = "Tusk";
+
+    private int level;
+
+
+    // Use this for initialization
+    void Start () {
+        weaponInput.text = "";
+        suspectInput.text = "";
+        level = 1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeHunchValue(string objectName, string objectTag)
+    {
+        if(objectTag == "Suspect")
+        {
+            suspectInput.text = objectName;
+        }
+        else if(objectTag == "Weapon")
+        {
+            weaponInput.text = objectName;
+        }
+    }
+
+    public void MakeHunch()
+    {
+        if ((weaponInput.text != "") && (suspectInput.text != ""))
+        {
+            //Comprobar si la sospecha es correcta
+            if((weaponInput.text == WinnerWeapon) && (suspectInput.text == WinnerSuspect))
+            {
+                WinGame();
+            }
+            //Si no, cargar siguiente nivel
+            else
+            {
+                level++;
+                LoadNewScene(level);
+            }
+        }
+    }
+
+    void WinGame()
+    {
+
+    }
+
+    IEnumerator LoadNewScene(int lvl)
+    {
+        //fade out the game and load a new level
+        float fadeTime = GameObject.Find("GameController").GetComponent<SceneFading>().BeginFade(1);
+        yield return new WaitForSeconds(fadeTime);
+        SceneManager.LoadScene(lvl);
+    }
+}
